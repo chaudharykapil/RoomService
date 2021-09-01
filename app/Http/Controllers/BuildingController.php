ee9895a00dcf48af398a920c337ecaf6dca24a5a
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-02 00:31:38 +0530

    add create building functionality

--- app/Http/Controllers/BuildingController.php
+++ app/Http/Controllers/BuildingController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Building;
 
 class BuildingController extends Controller
 {
@@ -13,5 +14,19 @@ class BuildingController extends Controller
     public function EditBuildingPage()
     {
         return view("pages/viewBuilding");
+    }
+    public function createBuilding(Request $req)
+    {
+        $build = new Building;
+        $data = $req->input();
+        $build->b_id = $data['build_id'];
+        $build->b_name = $data['build_name'];
+        try {
+            $build->status = $data['status'] == "on";
+        } catch (\Throwable $th) {
+            $build->status = false;
+        }
+        $build->save();
+        return redirect('/building/new');
     }
 }
