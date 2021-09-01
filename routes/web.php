963ef21f4771095ff91fa4fa658afd443495e05e
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-02 00:32:56 +0530

    add create room level and building route

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,7 @@ use App\Http\Controllers\AdminController;
 use App\Http\Controllers\RoomController;
 use App\Http\Controllers\BuildingController;
 use App\Http\Controllers\LevelController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+
 
 Route::get('/',[AdminController::class,'showLoginPage']);
 Route::post('/admin/login',[AdminController::class,'Login']);
@@ -23,8 +14,10 @@ Route::get('/admin/login/google/c',[AdminController::class,'GoogleCallback']);
 Route::get('/admin/logout',[AdminController::class,'LogOut']);
 Route::get('/room/new',[RoomController::class,'AddRoomPage']);
 Route::get('/room/edit',[RoomController::class,'EditRoomPage']);
+Route::post('/room/new',[RoomController::class,'createRoom']);
 Route::get('/building/new',[BuildingController::class,'AddBuildingPage']);
 Route::get('/building/edit',[BuildingController::class,'EditBuildingPage']);
+Route::post('/building/new',[BuildingController::class,'createBuilding']);
 Route::get('/level/new',[LevelController::class,'AddLevelPage']);
 Route::get('/level/edit',[LevelController::class,'EditLevelPage']);
-
+Route::post('/level/new',[LevelController::class,'createLevel']);
