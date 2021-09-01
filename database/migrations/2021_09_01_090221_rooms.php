84929f2811b456aaf15bd8f1041dfb7883f93a8b
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-02 00:33:33 +0530

    create rooms schema

--- database/migrations/2021_09_01_090221_rooms.php
+++ database/migrations/2021_09_01_090221_rooms.php
@@ -13,7 +13,15 @@ class Rooms extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('build_id',10);
+            $table->integer("level_no");
+            $table->integer("room_no");
+            $table->string('room_name',10);
+            $table->string('room_type',30);
+            $table->boolean("status");
+        });
     }
 
     /**
