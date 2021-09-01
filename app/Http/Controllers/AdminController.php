96b6f111bd741cca00f3e2e462846cfe71994dc0
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-01 21:52:14 +0530

    add google signin functionality

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,13 +1,64 @@
 <?php
-
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Admin;
+use Socialite;
 
 class AdminController extends Controller
 {
     public function showLoginPage()
     {
+        if(session("admin")){
+            return session("admin");
+        }
         return view("index");
+    }
+    public function Login(Request $req)
+    {
+        
+        $email = $req->email;
+        $pass = $req->password;
+        $adm = Admin::where("email","=",$email)->get();
+        if(count($adm)){
+            if ($adm[0]->password == $pass && $adm[0]->provider == '') {
+                session(["admin"=>$adm[0]->email]);
+                return redirect('/');
+            }
+            else {
+                return "login failed";
+            }
+        }
+        else {
+            return "login failed";
+        }
+    }
+    public function LogOut()
+    {
+        if (session("admin")){
+            session()->forget("admin");
+            return redirect('/');;
+        }
+        return "already logout";
+    }
+    public function redirectToProvider()
+    {
+        return Socialite::driver('google')->stateless()->redirect();
+    }
+    public function GoogleCallback()
+    {
+        $user = Socialite::driver('google')->stateless()->user();
+        $adm = Admin::where("email",'=',$user->getEmail())->get();
+        if (count($adm)){
+            session(["admin"=>$adm[0]->email]);
+            return redirect('/');
+        }
+        $newadm = new Admin;
+        $newadm->email =  $user->getEmail();
+        $newadm->password = "";
+        $newadm->provider = "google";
+        $newadm->save();
+        session(["admin"=>$user->getEmail()]);
+        return redirect('/');
+
     }
 }
