08f213e879f025ddc5cfac4fc83f4e6d44dc31d9
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-02 21:10:00 +0530

    add create edit and list room function

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,15 +3,56 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Room;
 
 class RoomController extends Controller
 {
     public function AddRoomPage()
     {
+        if(!session('admin')){
+            return redirect('/');
+        }
         return view("pages/newRoom");
     }
-    public function EditRoomPage()
+    public function createRoom(Request $req)
     {
-        return view("pages/viewRoom");
+        if(!session('admin')){
+            return redirect('/');
+        }
+        $data = $req->input();
+        $newRoom = new Room;
+        $newRoom->build_id = $data['build_id'];
+        $newRoom->level_no = $data['level_no'];
+        $newRoom->room_no = $data['room_no'];
+        $newRoom->room_name = $data['room_name'];
+        $newRoom->room_type = $data['room_type'];
+        try {
+            $newRoom->status = $data['status'] == 'on';
+        } catch (\Throwable $th) {
+            $newRoom->status = false;
+        }
+        $newRoom->save();
+
+        return redirect("/room/list");
+    }
+    public function EditRoomPage($id)
+    {
+        if(!session('admin')){
+            return redirect('/');
+        }
+        $room = Room::find($id);
+        if(!$room){
+            abort(404);
+        }
+        session()->flash("message","this is message");
+        return view("pages/editRoom",compact("room"));
+    }
+    public function ListRoomPage()
+    {
+        if(!session('admin')){
+            return redirect('/');
+        }
+        $all_rooms = Room::all();
+        return view("pages/roomList",compact("all_rooms"));
     }
 }
