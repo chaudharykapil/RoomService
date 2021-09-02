82b068615b8d644cddb97551c7abea7164f24dc6
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-02 22:26:08 +0530

    add update and delete function with flash message

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -10,13 +10,16 @@ class RoomController extends Controller
     public function AddRoomPage()
     {
         if(!session('admin')){
+            session()->flash("message","Please Login First");
             return redirect('/');
         }
         return view("pages/newRoom");
     }
+
     public function createRoom(Request $req)
     {
         if(!session('admin')){
+            session()->flash("message","Please Login First");
             return redirect('/');
         }
         $data = $req->input();
@@ -32,24 +35,66 @@ class RoomController extends Controller
             $newRoom->status = false;
         }
         $newRoom->save();
-
+        session()->flash("message","Room added");
         return redirect("/room/list");
     }
+
     public function EditRoomPage($id)
     {
         if(!session('admin')){
+            session()->flash("message","Please Login First");
             return redirect('/');
         }
         $room = Room::find($id);
         if(!$room){
             abort(404);
         }
-        session()->flash("message","this is message");
         return view("pages/editRoom",compact("room"));
+    }
+
+    public function updateRoom($id,Request $req)
+    {
+        if(!session('admin')){
+            session()->flash("message","Please Login First");
+            return redirect('/');
+        }
+        $room = Room::find($id);
+        if(!$room){
+            abort(404);
+        }
+        $data = $req->input();
+        $room->build_id = $data['build_id'];
+        $room->level_no = $data['level_no'];
+        $room->room_no = $data['room_no'];
+        $room->room_name = $data['room_name'];
+        $room->room_type = $data['room_type'];
+        try {
+            $room->status = $data['status'] == 'on';
+        } catch (\Throwable $th) {
+            $room->status = false;
+        }
+        $room->save();
+        session()->flash("message","Room update");
+        return redirect('/room/list');
+    }
+    public function deleteRoom($id,Request $req)
+    {
+        if(!session('admin')){
+            session()->flash("message","Please Login First");
+            return redirect('/');
+        }
+        $room = Room::find($id);
+        if(!$room){
+            abort(404);
+        }
+        $room->delete();
+        session()->flash("message","Room deleted");
+        return redirect('/room/list');
     }
     public function ListRoomPage()
     {
         if(!session('admin')){
+            session()->flash("message","Please Login First");
             return redirect('/');
         }
         $all_rooms = Room::all();
