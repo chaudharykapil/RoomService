dcfc320c8e4703dbcc5c1b790fbf8c91e76351b5
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-03 13:36:32 +0530

    Refector code

--- routes/web.php
+++ routes/web.php
@@ -6,27 +6,27 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\BuildingController;
 use App\Http\Controllers\LevelController;
 
-
+//<-----------------------------------------Routes for Admin ---------------------------->
 Route::get('/',[AdminController::class,'showLoginPage']);
 Route::post('/admin/login',[AdminController::class,'Login']);
 Route::get('/admin/login/provider',[AdminController::class,'redirectToProvider']);
 Route::get('/admin/login/google/c',[AdminController::class,'GoogleCallback']);
 Route::get('/admin/logout',[AdminController::class,'LogOut']);
-
+//<-----------------------------------------Routes for Rooms ---------------------------->
 Route::get('/room/new',[RoomController::class,'AddRoomPage']);
 Route::post('/room/new',[RoomController::class,'createRoom']);
 Route::get('/room/edit/{id}',[RoomController::class,'EditRoomPage']);
 Route::post('/room/edit/{id}',[RoomController::class,'updateRoom']);
 Route::get('/room/list',[RoomController::class,'ListRoomPage']);
 Route::get('/room/delete/{id}',[RoomController::class,'deleteRoom']);
-
+//<-----------------------------------------Routes for Building ---------------------------->
 Route::get('/building/new',[BuildingController::class,'AddBuildingPage']);
 Route::post('/building/new',[BuildingController::class,'createBuilding']);
 Route::get('/building/edit/{id}',[BuildingController::class,'EditBuildingPage']);
 Route::post('/building/edit/{id}',[BuildingController::class,'EditBuildingPage']);
 Route::get('/building/list',[BuildingController::class,'ListBuildingPage']);
 Route::get('/building/delete/{id}',[BuildingController::class,'EditBuildingPage']);
-
+//<-----------------------------------------Routes for Levels ---------------------------->
 Route::get('/level/new',[LevelController::class,'AddLevelPage']);
 Route::post('/level/new',[LevelController::class,'createLevel']);
 Route::get('/level/edit/{id}',[LevelController::class,'EditLevelPage']);
