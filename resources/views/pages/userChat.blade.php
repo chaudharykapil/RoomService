e7166669ac185a51e22e560c8123eabd63409e2f
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-27 23:56:30 +0530

    fix UI

--- resources/views/pages/userChat.blade.php
+++ resources/views/pages/userChat.blade.php
@@ -8,26 +8,19 @@
     <title>Document</title>
     <script src="{{asset('js/app.js')}}"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script src="{{asset('js/chatSystem.js')}}"></script>
+    <script src="{{asset('js/UserChatSystem.js')}}"></script>
 </head>
 <body>
     <div>
         <div>
             <h1>Messages</h1>
-            <ul></ul>
+            <ul id="msglist">
+            </ul>
         </div>
         <div>
             <input type="text" id="msgbox" placeholder="Type Message here . . .">
-            <button type="button" onclick="sendMessage({{$send_to}},2)">Send</button>
+            <button type="button" onclick="SendMessage({{$send_to}},2)">Send</button>
         </div>
-        <form action="/chat" method="post">
-            @csrf
-            <input type="text" name="message">
-            <input type="text" name="sender_id">
-            <input type="text" name="send_to">
-            <button type="submit">send</button>
-
-        </form>
     </div>
 </body>
 </html>
