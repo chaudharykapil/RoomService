49ad5f53e1ea2351aa72c640f035f66f8bd23e67
chaudharykapil <chaudharykapil8279@gmail.com>
2021-09-27 23:55:17 +0530

    add chat controller

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Events\MessageSend;
+class ChatController extends Controller
+{
+    public function SendMessage(Request $req)
+    {
+        $data = $req->input();
+        $msg = new Message;
+        $msg->sender_id = $data['sender_id'];
+        $msg->reciever_id = $data['send_to'];
+        $msg->message = $data['message'];
+        if ($data["message"] == null) {
+            $msg->message = "";
+        }        
+        $msg->save();
+        broadcast(new MessageSend($msg))->toOthers();
+        return "done";
+    }
+}
