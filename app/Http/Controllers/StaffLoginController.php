64af748ff7eaedc30c10e2f126feaa87c822651c
chaudharykapil <chaudharykapil8279@gmail.com>
2021-10-02 13:13:49 +0530

    add staff login logout funtionality

--- app/Http/Controllers/StaffLoginController.php
+++ app/Http/Controllers/StaffLoginController.php
@@ -3,11 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Staff;
 class StaffLoginController extends Controller
 {
     public function ShowLoginPage()
     {
+        if(session("staff")){
+            return redirect('/staff/allbooking');
+        }
         return view("staff/staffLogin");
+    }
+    public function StaffLogin(Request $req)
+    {
+        if(session("staff")){
+            return redirect('/staff/allbooking');
+        }
+        $data = $req->input();
+        $staff_id = $data["staff_id"];
+        $password = $data["password"];
+        $staff = Staff::Where("staff_id","=",$staff_id)->Where("password","=",$password)->get();
+        if(count($staff)>0){
+            session(["staff"=>$staff[0]]);
+            return redirect("/staff/login");
+        }
+        else{
+            session()->flash("message","Login Failed");
+            return redirect('/staff/login');
+        }
+    }
+
+    public function StaffLogout()
+    {
+        if(session("staff")){
+            session()->forget("staff");
+        }
+        else {
+            session()->flash("message","Already LogOut");
+        }
+        return redirect('/staff/login');
     }
 }
